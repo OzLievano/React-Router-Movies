847f2451dca680dfe3eaabf794e93adf3e9e6ab7
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-09-30 22:22:35 -0400

    cards link to a page

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function MovieList(props) {
   console.log(props)
@@ -7,23 +8,24 @@ export default function MovieList(props) {
       {props.data.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
-      placeholder
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { key,title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+      <div className="movie-card">
+        <Link to={`/movies/${key}`}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        </Link>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
   );
 }
